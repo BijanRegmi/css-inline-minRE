b5f595c35d91a3b3c266cffa9b4d93227952942d
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2024-05-11 15:16:26 +0545

    remove timeout

--- test.spec.js
+++ test.spec.js
@@ -1,12 +1,6 @@
 const { inline } = require("@css-inline/css-inline");
 
 describe("css-inline", () => {
-    jest.setTimeout(30000);
-
-    afterAll(async () => {
-        await new Promise((resolve) => setTimeout(() => resolve(), 10000)); // avoid jest open handle error
-    });
-
     it("nop", () => {
         const document = `<html><head></head><body></body></html>`;
         const expected = `<html><head></head><body></body></html>`;
